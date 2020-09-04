c1f84b80bd271442089cd935c23b9d9d4a2c34b9
morgancrw <mcrew@colgate.edu>
2020-09-04 09:39:10 -0700

    vector practice

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,28 @@
 print("Hello World")
+
+#six raised to the power of six
+6^6
+
+#234 + 8
+234+8
+
+#15-28
+15-28
+
+#name my number
+m <- 6234
+m*5
+m/3
+
+
+#vector1
+a <- c(122, 34, 19290, 334)
+a/2
+
+#vector 2
+b <- c(12334,322,421,2)
+b*10
+
+#vector 3
+c <- c(8,3,12,44)
+c+2
