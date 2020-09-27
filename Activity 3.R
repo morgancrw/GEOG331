9c04a3c28be4ae0f2ee8b77a96d1774561251b25
Morgan <mcrew@colgate.edu>
2020-09-27 16:06:05 -0400

    Question 6

--- Activity 3.R
+++ Activity 3.R
@@ -1,5 +1,6 @@
 #install lubridate package
 #install.packages(c("lubridate"))
+library(lubridate)
 
 #create assert function
 # assert <- function(statement, err.message) {
@@ -34,13 +35,13 @@ colnames(datW) <- colnames(sensorInfo)
 #preview data
 #print(datW[1,])
 
-#convert to m/d/y
+# #convert to m/d/y
 dates <- mdy_hm(datW$timestamp, tz = "America/New_York")
-#calculate day of year
+# #calculate day of year
 datW$doy <- yday(dates)
-#calculate hour in the day
+# #calculate hour in the day
 datW$hour <- hour(dates) + (minute(dates)/60)
-#calculate decimal day of year
+# #calculate decimal day of year
 datW$DD <- datW$doy + (datW$hour/24)
 
 # #find number of missing values for air temp
@@ -70,8 +71,45 @@ datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
 #quantile(datW$air.tempQ1)
 
 #look at days with really low air temp
-datW[datW$air.tempQ1 < 8,]
+#datW[datW$air.tempQ1 < 8,]
 #look at days with really high air temp
-datW[datW$air.tempQ1 > 33,]
+#datW[datW$air.tempQ1 > 33,]
+
+
+# #plot precipitation and lightning strikes on the same plot
+# #normalize lightning strikes to match precipitation
+# lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * 
+#   datW$lightning.acvitivy
+# #make plot with precipitation adn ligtning activity marked
+# #make it empty to start and add in features
+# plot(datW$DD, datW$precipitation, xlab="Day of Year", ylab="Precipitation & Lightning",
+#      type = "n")
+# #plot semitransparent points for precipitation > 0 
+# points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0], 
+#        col = rgb(95/255, 158/255, 160/255, .5), pch = 15)
+# #plot lighning points where there is lightning
+# points(datW$DD[lightscale > 0], lightscale[lightscale > 0], col="tomato3", pch=19)
+
+#filter out storms in wind and air temperature measurements
+#filter all values with lightning that coincides with rainfall grather than 2mm 
+#       or only rainfal over 5mm
+#create a new air temp column
+
+# datW$air.tempQ2 <- ifelse(datW$precipitation >= 2 & datW$lightning.acvitivy > 0,
+#                           NA, ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+#------------------QUESTION 8----------------------
+#create data frame for researchers requested values and add total precipitation
+datSummary <- data.frame("totalPrecipitation" = sum(datW$precipitation, na.rm =TRUE))
+#fill data frame with average air temp, windspeed, soil moisture, and soil temp
+datSummary$aveAirTemp <- mean(datW$air.temperature, na.rm = TRUE)
+datSummary$aveWindSpeed <- mean(datW$wind.speed, na.rm=TRUE)
+datSummary$aveSoilMoist <- mean(datW$soil.moisture, na.rm=TRUE)
+datSummary$aveSoilTemp <- mean(datW$soil.temp, na.rm = TRUE)
+
+str(datSummary)
+
+#---------------------------------------------------
+
 
 
