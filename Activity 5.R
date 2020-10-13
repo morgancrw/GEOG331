36b6ec62d5813b7a467f7a39ca8be72115640f43
Morgan <mcrew@colgate.edu>
2020-10-13 00:26:29 -0400

    Question 9

--- Activity 5.R
+++ Activity 5.R
@@ -1,5 +1,7 @@
 #load in lubridate
 library(lubridate)
+library(dplyr)
+library(ggplot2)
 
 #read in streamflow data
 datH <- read.csv("C:\\Users\\morga\\OneDrive\\Documents\\GitHub\\GEOG331\\stream_flow_data.csv", na.strings = c("Eqp"))
@@ -105,7 +107,85 @@ lines(obsYear$doy, obsYear$discharge, lwd=2, col=c("grey71"))
 
 #-----------------------QUESTION 7------------------------
 
+# 
+# tempFrame <- datP %>% 
+#         group_by(datP$doy, datP$year) %>%
+#         summarise(count = n())
+# everyHour <- subset(tempFrame, tempFrame$count==24)
+# 
+# plot(datD$doy, datD$discharge, type="l", xlab="Year", 
+#      ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+
+#--------------------------------------------------------------
 
 
+#-----------------QUESTION 8----------------
 
+# 
+# hydroD <- datD[datD$doy >= 1 & datD$doy < 3 & datD$year == 2012,]
+# hydroP <- datP[datP$doy >= 1 & datP$doy < 3 & datP$year == 2012,]
+# 
+# #get minimum and maximum range of discharge to plot
+# #go outside of the range so that it's easy to see high/low values
+# #floor rounds down the integer
+# yl <- floor(min(hydroD$discharge))-1
+# #ceiling rounds up to the integer
+# yh <- ceiling(max(hydroD$discharge))+1
+# #minimum and maximum range of precipitation to plot
+# pl <- 0
+# pm <-  ceiling(max(hydroP$HPCP))+.5
+# #scale precipitation to fit on the 
+# hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+# 
+# 
+# par(mai=c(1,1,1,1))
+# #make plot of discharge
+# plot(hydroD$decDay,
+#      hydroD$discharge, 
+#      type="l", 
+#      ylim=c(yl,yh), 
+#      lwd=2,
+#      xlab="Day of year", 
+#      ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+# 
+# #add bars to indicate precipitation 
+#  for(i in 1:nrow(hydroP)){
+#          polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+#                   hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+#                  c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+#                  col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+#  }
+#---------------------------------------------------------
+#-----------------QUESTION 9-----------------------------------
+#add months to datD dataframe
+datD$month <- month(datesD)
 
+#create dataframe for only 2017
+obsYear2017 <- data.frame(datD$discharge[datD$year==2017], 
+                      datD$doy[datD$year==2017], datD$year[datD$year==2017],
+                      datD$month[datD$year==2017])
+colnames(obsYear2017) <- c("discharge", "doy", "year", "month")
+
+#create dataframe for only 2016
+obsYear2016 <- data.frame(datD$discharge[datD$year==2016], 
+                          datD$doy[datD$year==2016], datD$year[datD$year==2016],
+                          datD$month[datD$year==2016])
+colnames(obsYear2016) <- c("discharge", "doy", "year", "month")
+
+#add seasons for 2017
+obsYear2017$season <- ifelse((obsYear2017$month < 3) | (obsYear2017$month == 12) , "winter", 
+                             ifelse((obsYear2017$month >= 3) & (obsYear2017$month <6), "spring",
+                            ifelse((obsYear2017$month >=6) & (obsYear2017$month<9), "summer",
+                            ifelse((obsYear2017$month >=9) & (obsYear2017$month<12), "fall", NA))))
+#violon plot for 2017
+ggplot(data = obsYear2017, aes(season, discharge)) + geom_violin()
+
+#add seasons for 1016
+obsYear2016$season <- ifelse((obsYear2016$month < 3) | (obsYear2016$month == 12) , "winter", 
+                             ifelse((obsYear2016$month >= 3) & (obsYear2016$month <6), "spring",
+                                ifelse((obsYear2016$month >=6) & (obsYear2016$month<9), "summer",
+                                ifelse((obsYear2016$month >=9) & (obsYear2016$month<12), "fall", NA))))
+
+#make violin plot for 2016
+ggplot(data = obsYear2016, aes(season, discharge)) + geom_violin()
+#-------------------------------------------------------------------------
