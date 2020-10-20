e47eea215b787397ed8cfeab5ccffc6a5f2fadac
Morgan <mcrew@colgate.edu>
2020-10-20 12:45:35 -0400

    Read in Data

---
+++ Project.R
@@ -0,0 +1,7 @@
+
+#read in data
+datDrought <- read.csv("C:\\Users\\morga\\OneDrive\\Documents\\GitHub\\GEOG331\\dm_export_20191006_20201006.csv", 
+                 stringsAsFactors = T)
+datFire <- read.csv("C:\\Users\\morga\\OneDrive\\Documents\\GitHub\\GEOG331\\Monitoring_Trends_in_Burn_Severity_Fire_Occurrence_Locations__Feature_Layer_.csv", 
+                            stringsAsFactors = T)
+
