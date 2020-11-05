e1ed81e04219c03957bb1d9bc5a3c574aec323f7
morgancrw <60332611+morgancrw@users.noreply.github.com>
2020-11-04 19:30:16 -0500

    Question 11

--- Activity 6.R
+++ Activity 6.R
@@ -41,16 +41,16 @@ for(i in 1:length(ndviYear)){
 }
 
 #---------------------------QUESTION 3---------------------------
-# #plot both side by side
-# par(mfrow=c(1,2))
-# #plot with color
-# #show axes for reference
-# #add contrast to the imagery to see it better
-# par(mai=c(1,1,1,1))
-# plotRGB(rgbL, stretch="lin", axes=TRUE)
-# #add polygons to plot
-# plot(g1966, col="tan3", border=NA, add=TRUE)
-# plot(NDVIraster[[1]])
+#plot both side by side
+par(mfrow=c(1,2))
+#plot with color
+#show axes for reference
+#add contrast to the imagery to see it better
+par(mai=c(1,1,1,1))
+plotRGB(rgbL, stretch="lin", axes=TRUE)
+#add polygons to plot
+plot(g1966, col="tan3", border=NA, add=TRUE)
+plot(NDVIraster[[1]])
 #---------------------------------------------------------------
 
 #reproject the glaciers
@@ -62,8 +62,8 @@ g2015p <- spTransform(g2015,NDVIraster[[1]]@crs)
 
 #------------------------QUESTION 4---------------------
 # #map maximum NDVI and glaciers in 2015
-# plot(NDVIraster[[13]], axes=FALSE)
-# plot(g2015p, border="black", add=TRUE)
+plot(NDVIraster[[13]], axes=FALSE)
+plot(g2015p, border="black", add=TRUE)
 #-------------------------------------------------------
 
 #calculate area for all polygons
@@ -99,47 +99,112 @@ gChange$areaC <- pct(gChange$a1966m.sq, gChange$a2015m.sq)
 gChange$areaC <- sprintf(gChange$areaC, fmt = '%#.2f')
 gChange$areaC <- as.numeric(gChange$areaC)
 
-#g2015$change <- gChange$areaC
-#head(g2015@data)
-#spplot(g2015, "change")
-#CREATE SPPLOT
+g2015$change <- gChange$areaC
+
+spplot(g2015, "change")
+
 
 #----------------------------------------------------
 
 #-------------------QUESTION 6--------------------------
 
 #find glacier with largest % loss
-gChange$Name[gChange$areaC == min(gChange$areaC)]
+change <- gChange$Name[gChange$areaC == min(gChange$areaC)]
+percChange <- min(gChange$areaC)
+newMap1966 <- subset(g1966p, g1966p$GLACNAME==change)
+newMap1998 <- subset(g1998p, g1966p$GLACNAME==change)
+newMap2005 <- subset(g2005p, g1966p$GLACNAME==change)
+newMap2015 <- subset(g2015p, g1966p$GLACNAME==change)
+
+
+par(mai=c(1,1,1,1))
+plotRGB(rgbL, stretch="lin", axes=TRUE, main=paste("% change:", percChange, "% Glacier: ", change))
+plot(newMap1966, col="lightblue2", add=TRUE, border=NA)
+plot(newMap1998, col="green", add=TRUE, border=NA)
+plot(newMap2005, col="mediumorchid1", add=TRUE, border=NA)
+plot(newMap2015, col="grey1", add=TRUE, border=NA)
+
 
 #-------------------------------------------------------
 #------------------QUESTION 7------------------------
 # #designate that NDVIraster list is a stack
-# NDVIstack <- stack(NDVIraster)
+  NDVIstack <- stack(NDVIraster)
+# #
+#set up lm function to apply to every cell
+  timeT <- ndviYear
+  fun <- function(x) {
+   if(is.na(x[1])){
+      NA}else{
+        #fit a regression and extract a slope
+        lm(x ~ timeT)$coefficients[2] }}
 # 
-# #set up lm function to apply to every cell
-# timeT <- ndviYear
-# fun <- function(x) {
-#   if(is.na(x[1])){
-#     NA}else{
-#       #fit a regression and extract a slope
-#       lm(x ~ timeT)$coefficients[2] }}
-# 
-# #apply the slope function to the rasters
-# NDVIfit <- calc(NDVIstack,fun)
-# 
-# #plot the change in NDVI
-# plot(NDVIfit, axes=FALSE)
+# # #apply the slope function to the rasters
+NDVIfit <- calc(NDVIstack,fun)
+
+#plot the change in NDVI
+plot(NDVIfit, axes=FALSE)
 #------------------------------------------------------
 #-----------------------QUESTION 8-------------------------
-#buffer glaciers
-glacier500m <- gBuffer(g1966p,
+# #buffer glaciers
+ glacier500m <- gBuffer(g1966p,
                        byid=TRUE,
-                       width=500)
-#convert to a raster
+                        width=500)
+# #convert to a raster
 buffRaster <- rasterize(glacier500m,
-                        NDVIraster[[1]], 
-                        field=glacier500m@data$GLACNAME, 
-                        background=0)
+                         NDVIraster[[1]],
+                         field=glacier500m@data$GLACNAME,
+                         background=0)
 plot(buffRaster)
+# 
+# #rasterize gralciers
+glacRaster <- rasterize(g1966p, NDVIraster[[1]], field=g1966p@data$GLACNAME, background=0)
+# 
+# #subtract buffer from original glacier
+glacZones <- buffRaster - glacRaster
+plot(glacZones)
 
-#-----------------------------------------------------------
+#------------------------------------------------------------
+#-----------------------QUESTION 9--------------------------------
+meanChange <- zonal(NDVIfit, glacZones, "mean")
+g2015p@data$zmean<-meanChange[-(1),]
+
+#add means to glacier polygons
+g2015p@data$zmean <- 0
+
+for(i in 1:39) {
+  g2015p@data$zmean[i] = meanChange[i+1,2]
+}
+
+spplot(g2015p, "zmean")
+
+
+#------------------------------------------------------
+#--------------------QUESTION 11------------------------
+rmean <- calc(NDVIstack, mean)
+
+NDVImean <- list()
+for (i in 1:length(ndviYear)){
+  NDVImean[i] <- cellStats(NDVIraster[[i]], stat="mean", na.rm=TRUE)
+}
+
+mean(unlist(NDVImean))
+
+#find NDVI range
+NDVIfitmin <- calc(NDVIstack, min)
+NDVIfitmax <- calc(NDVIstack, max)
+NDVIfitrange <- NDVIfitmax - NDVIfitmin
+
+#find NDVI range per polygon
+averange <- raster::extract(NDVIfitrange, g2015p, buffer=0, df=TRUE)
+g2015p@data$averange1 <- 0
+for (i in 1:39){
+  g2015p@data$averange1[i]=averange[i,2]
+}
+
+#plot NDVI color coated polygons
+spplot(g2015p, "averange1")
+
+#plot NDVI average
+plot(rmean)
+
+#-------------------------------------------------------
